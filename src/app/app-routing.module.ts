207992edd579729849cbd3ff857b534eb88b06c1
Alexandre Soffiatti <alexandre.santos@iesde.com.br>
2023-01-17 11:49:54 -0300

    Define title page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+@Injectable({ providedIn: 'root' })
+
+export class TemplatePageTitleStrategy extends TitleStrategy{
+  constructor(private readonly title: Title){
+    super()
+  }
+
+  override updateTitle(routeState: RouterStateSnapshot){
+    const title = this.buildTitle(routeState)
+    if(title !== undefined){
+      this.title.setTitle(`UEX Contacts | ${title}`)
+    }
+  }
+
+}
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }
+  ]
 })
 export class AppRoutingModule { }
