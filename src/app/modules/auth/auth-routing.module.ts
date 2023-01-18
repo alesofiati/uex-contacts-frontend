5c32014743584ac2ab101b4d7a90b5d7ccfbf3c8
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2023-01-17 22:57:51 -0300

    page forgot password

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'login', title: 'Login', component:LoginComponent },
-  { path: 'register', title: 'Cadastro', component:RegisterComponent }
+  { path: 'register', title: 'Cadastro', component:RegisterComponent },
+  { path: 'forgot-password', title: 'Esqueceu a Senha', component:ForgotPasswordComponent },
 ];
 
 @NgModule({
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,5 +1,3 @@
-import { LabelComponent } from './../../components/label/label.component';
-import { InputComponent } from './../../components/input/input.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,6 +5,11 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+
+//components
+import { LabelComponent } from './../../components/label/label.component';
+import { InputComponent } from './../../components/input/input.component';
 
 
 @NgModule({
@@ -15,7 +18,8 @@ import { ButtonComponent } from 'src/app/components/button/button.component';
     RegisterComponent,
     ButtonComponent,
     InputComponent,
-    LabelComponent
+    LabelComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
