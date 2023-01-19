488f750c0daf93f553d60b75a82453475a9a9216
Alexandre Soffiatti <alexandre.santos@iesde.com.br>
2023-01-19 11:50:15 -0300

    define a route for reset-password component

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'login', title: 'Login', component:LoginComponent },
   { path: 'register', title: 'Cadastro', component:RegisterComponent },
   { path: 'forgot-password', title: 'Esqueceu a Senha', component:ForgotPasswordComponent },
+  { path: 'reset-password/token/:token', title: 'Recuperar Senha', component:ResetPasswordComponent },
 ];
 
 @NgModule({
