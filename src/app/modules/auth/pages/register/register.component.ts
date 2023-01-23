d7a29d58871cf53589cc297864a016e4f49a9f0a
Alexandre Soffiatti <alexandre.santos@iesde.com.br>
2023-01-23 17:11:29 -0300

    init validation form

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder){
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [
+          Validators.required, Validators.email
+        ]
+      ],
+      password: ['', Validators.required],
+      password_confirmation: ['', [
+          Validators.required, Validators.minLength(4), Validators.maxLength(8)
+        ]
+      ]
+    })
+  }
+
+  ngOnInit():void {}
+
+  onSubmit()
+  {
+
+    if(!this.registerForm.valid){
+      alert('O formulario possui campos obrigatorios')
+      return
+    }
+
+    console.log(this.registerForm.value)
+  }
 
 }
